622c0a12a44fabd4f9ceaae8e107baf0c310c938
Alejo Boga <alejoboga20@gmail.com>
2022-04-03 20:27:12 -0300

    Implementing flat list

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
 
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
