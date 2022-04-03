faefb5051cb30d37b8328c5a1af5fb36c824cee4
Alejo Boga <alejoboga20@gmail.com>
2022-04-03 20:10:53 -0300

    Setup navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Navigator } from './src/Navigator/Navigator';
 
 export const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 };
